375e0ab9d6c8beaa5c516a300ebd94526d379f84
Bhagyashri0411 <bhagyashripawar0411@gmail.com>
2022-08-16 21:02:27 +0530

    done

--- templates/dashboard/dashboard.js
+++ templates/dashboard/dashboard.js
@@ -13,6 +13,15 @@ function opendiv() {
 function closediv() {
   document.getElementById('openmodel').style.display = "none";
 }
+
+function openserverdiv() {
+  document.getElementById('openservermodel').style.display = "block";
+}
+function closeserverdiv() {
+  document.getElementById('openservermodel').style.display = "none";
+}
+
+
 function myFunction() {
   var dots = document.getElementById("dots");
   var moreText = document.getElementById("more");
@@ -87,4 +96,46 @@ function myFunction2() {
     }
   });
 }
+function showdivdrop() {
+  document.getElementById('showdropdiv').style.display = 'block'
 
+}
+function usersubdata() {
+  console.log('hii');
+  var usertext = $('#usertextfield').val();
+  var usermail = $('#usermailfield').val();
+  var usermobile = $('#usermobilefield').val();
+  var usermes = $('#usermesfield').val();
+
+  if (usertext == '', usermail == '', usermobile == '', usermes == '') {
+    document.getElementById('messages').innerText = "Please Enter The Values";
+    document.getElementById('messhow').style.display = 'block';
+  }
+  else {
+    var myData = { 'id': null, 'user_name': usertext, 'mail': usermail, 'mob_no': usermobile, 'message': usermes }
+
+    $.ajax({
+      type: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: JSON.stringify(myData),
+      url: "http://192.168.46.105:8090/enq/books",
+      success: function (msg) {
+        if (msg == "Inserted Sucessfully") {
+          document.getElementById('messages').innerText = "Successful";
+          document.getElementById('messhow').style.display = 'block';
+          // document.getElementById('openmodel').style.display = "none";
+          document.getElementById('resetourdata').reset();
+
+        }
+        else {
+          document.getElementById('messages').innerText = "Sucessfull Failed";
+        }
+      }
+    })
+
+  }
+
+  $
+}
